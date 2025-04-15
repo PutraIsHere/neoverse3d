b7d8da6f4858661200fc0985197a270cccbba70e
PutraIsHere <129611806+PutraIsHere@users.noreply.github.com>
2025-04-16 03:17:47 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   webpack: (config) => {
     config.resolve.alias['@tensorflow/tfjs'] = '@tensorflow/tfjs/dist/tf.min.js'
+    config.resolve.alias['three'] = 'three/build/three.min.js'
     return config
   }
 }
